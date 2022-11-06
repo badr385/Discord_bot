49efa6b47cdaf7012d3551eca0e5d9ba844aa864
badr385 <59146833+badr385@users.noreply.github.com>
2022-11-06 20:18:04 +0100

    Create lucky.py
    it was a bot giveway to show a winner.
    
    Needed arguments :
    
    lucky nameofrole where name of role is which role you wanna launch the giveaway

---
+++ lucky.py
@@ -0,0 +1,91 @@
+import discord
+import os
+import random
+from discord.ext import commands
+from dotenv import load_dotenv
+import datetime
+
+load_dotenv()
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command('help')
+x = datetime.datetime.now()
+month = x.strftime("%B")
+year = x.strftime("%Y")
+
+
+@bot.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(bot))
+
+
+ 
+@bot.command(pass_context=True)
+async def lucky(ctx, rolename):
+#getting participant on a rolename of ur actual server and choosing one of them randomly
+	for guild in bot.guilds:
+		for row in guild.roles:
+			if row.name == rolename:
+				roleid = row.id
+				role = guild.get_role(roleid)
+				participant = role.members
+				guildname = role.guild.name
+				names = []        
+				for row in participant:
+					names.append(row.name)    
+        Winner = random.choice(names)
+				for row2 in participant:
+					if row2.name == Winner:
+						Wid = row2.id
+						user = await ctx.bot.fetch_user(Wid)
+						avatar = user.avatar_url
+      
+  #building discord core message
+	embed = discord.Embed(color=0x5e0303)
+	embed.set_author(
+	    name="Monthly Giveaway of  " + guildname,
+	    icon_url="Icon url"
+	)
+	embed.add_field(name="List of Participant:",
+	                value=', '.join(names),
+	                inline=True)
+	embed.add_field(name="Winner of the Giveaway this month " + month + "/" +
+	                year + " is : ",
+	                value="🤴 " + "<@" + str(Wid) + ">",
+	                inline=False)
+	embed.add_field(name="Check here the winner ",
+	                value="<@&" + str(roleid) + ">",
+	                inline=False)
+	embed.set_footer(text="🎉 😎 ✨🥳 We are pleased u won This Month  ",
+	                 icon_url=avatar)
+	await ctx.send(embed=embed)
+
+
+@bot.command(pass_context=True)
+async def lhelp(ctx):
+	embed = discord.Embed(title="How to use little Lucky <3",
+	                      description="Commands help",
+	                      color=0x00ff00)
+	embed.add_field(name="lhelp",
+	                value="Show u available commands",
+	                inline=False)
+	embed.add_field(
+	    name=
+	    "lucky nameofrole, where namerole groupe that u want to do the giveaway in",
+	    value="Launch the draw of the giveaway",
+	    inline=True)
+	await ctx.send(embed=embed)
+
+
+@lucky.error
+async def tenor_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send(
+		    'Rolename missing, use command lhelp to get all available commands'
+		)
+	else:
+		raise error
+
+
+bot.run(os.getenv('DISCORD_TOKEN'))
